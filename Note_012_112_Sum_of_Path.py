95457b587a98ba3c345934ce0db1b6b8709a473e
Junqi Li <745003837@qq.com>
2022-10-24 22:35:10 +0800

    Note 012

--- Note_012_112_Sum_of_Path.py
+++ Note_012_112_Sum_of_Path.py
@@ -26,15 +26,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-#         if not root:
-#             return False
-#         if root.left is None and root.right is None and root.val == targetSum:
-#             return True
-#         left = self.hasPathSum(root.left, targetSum - root.val)
-#         right = self.hasPathSum(root.right, targetSum - root.val)
-#         return left or right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if root.left is None and root.right is None and root.val == targetSum:
+            return True
+        left = self.hasPathSum(root.left, targetSum - root.val)
+        right = self.hasPathSum(root.right, targetSum - root.val)
+        return left or right
 
 
 """
