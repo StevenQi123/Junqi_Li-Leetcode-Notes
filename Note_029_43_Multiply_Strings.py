fa0f095d6806759c5e37906b3c4fbfb93b464ced
Junqi Li <745003837@qq.com>
2022-11-02 17:00:58 +0800

    Note 029

---
+++ Note_029_43_Multiply_Strings.py
@@ -0,0 +1,34 @@
+"""
+    来源：力扣（LeetCode）
+    链接：https://leetcode.cn/problems/multiply-strings
+    难度：中等
+    解法：竖式乘法模拟
+    题目描述：
+    给定两个以字符串形式表示的非负整数num1和num2，返回num1和num2的乘积，它们的乘积也表示为字符串形式。
+    注意：不能使用任何内置的 BigInteger库或直接将输入转换为整数。
+
+    Input: num1 = "2", num2 = "3"
+    Output: "6"
+
+"""
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        m, n = len(num1), len(num2)
+        ansArr = [0] * (m + n)
+        for i in range(m - 1, -1, -1):
+            x = int(num1[i])
+            for j in range(n - 1, -1, -1):
+                ansArr[i + j + 1] += x * int(num2[j])
+
+        for i in range(m + n - 1, 0, -1):
+            ansArr[i - 1] += ansArr[i] // 10
+            ansArr[i] %= 10
+
+        index = 1 if ansArr[0] == 0 else 0
+        ans = "".join(str(x) for x in ansArr[index:])
+        return ans
